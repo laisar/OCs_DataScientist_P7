0d06d6fd0c4febdd3519f1986ee4b06b17b855fc
laisar <lais.amorim.reis@gmail.com>
2024-01-08 19:26:07 +0100

    Update 2_🔎_Local analysis.py

--- app2/pages/2_🔎_Local analysis.py
+++ app2/pages/2_🔎_Local analysis.py
@@ -30,7 +30,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-
+st.write(path)
 ########################################################
 # Page information
 ########################################################
